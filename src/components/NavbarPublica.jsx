8f2c42222b1060e4fe5ce9019b12dc4b17bc76fe
Raniel <rnldourado@gmail.com>
2026-03-07 21:07:25 -0300

    feat: change register word in button to white

--- src/components/NavbarPublica.jsx
+++ src/components/NavbarPublica.jsx
@@ -8,7 +8,7 @@ export default function NavbarPublica() {
         <Link to="/login" className="btn-outline" style={{ padding: '8px 20px' }}>
           Entrar
         </Link>
-        <Link to="/cadastro/cliente" className="btn-primary" style={{ padding: '8px 20px', borderRadius: '10px', textDecoration: 'none', fontSize: '14px' }}>
+        <Link to="/cadastro/cliente" className="btn-primary" style={{ padding: '8px 20px', borderRadius: '10px', textDecoration: 'none', fontSize: '14px', color: 'white'}}>
           Cadastrar
         </Link>
       </div>
